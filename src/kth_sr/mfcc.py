4412b33c541a0297291fcc3a5323c554f8e2db2e
Jan Cap <vorel007@gmail.com>
2025-02-26 15:34:18 +0100

    add first_k_from_file

--- src/kth_sr/mfcc.py
+++ src/kth_sr/mfcc.py
@@ -1,5 +1,6 @@
 import numpy as np
 from deep_speaker.audio import pad_mfcc, mfcc_fbank
+import librosa
 
 
 def first_k_windows(audio, sample_rate: int, num_frames: int, k: int = 5):
@@ -37,3 +38,21 @@ def first_k_windows(audio, sample_rate: int, num_frames: int, k: int = 5):
         segments.append(mfcc[start_frame:end_frame])
 
     return np.array(segments).reshape((-1, 160, 64, 1))
+
+
+def first_k_windows_from_file(
+    file_path: str, sample_rate: int, num_frames: int, k: int = 5
+):
+    """Return first k windows of the Mel-filterbank energy features from a file.
+    Args:
+        file_path (str): Path to the audio file.
+        sample_rate (int): Sample rate of the audio.
+        num_frames (int): Number of frames in each window.
+        k (int): Number of frames to return.
+
+    Returns:
+        np.ndarray: Array of first k frames prepared for the model.
+        Dimensions are (k, num_frames, 64, 1)
+    """
+    audio, _ = librosa.load(file_path, sr=sample_rate)
+    return first_k_windows(audio, sample_rate, num_frames, k)
