884d3f0fb807c69527d4e160773a30bcb89995d8
Jan Cap <vorel007@gmail.com>
2025-02-26 15:32:37 +0100

    update first_k_window input

--- src/kth_sr/mfcc.py
+++ src/kth_sr/mfcc.py
@@ -1,12 +1,12 @@
 import numpy as np
-from deep_speaker.audio import pad_mfcc
+from deep_speaker.audio import pad_mfcc, mfcc_fbank
 
 
-def first_k_windows(mfcc: np.ndarray, num_frames: int, k: int = 5):
-    """Return first k frames of the mfcc array.
-
+def first_k_windows(audio, sample_rate: int, num_frames: int, k: int = 5):
+    """Return first k windows of the Mel-filterbank energy features.
     Args:
-        mfcc (np.ndarray): MFCC array of processed audio.
+        audio (np.ndarray): Audio array.
+        sample_rate (int): Sample rate of the audio.
         num_frames (int): Number of frames in each window.
         k (int): Number of frames to return.
 
@@ -14,13 +14,26 @@ def first_k_windows(mfcc: np.ndarray, num_frames: int, k: int = 5):
         np.ndarray: Array of first k frames prepared for the model.
         Dimensions are (k, num_frames, 64, 1)
     """
-    frames = []
+
+    energy = np.abs(audio)
+    silence_threshold = np.percentile(energy, 95)
+    offsets = np.where(energy > silence_threshold)[0]
+    # left_blank_duration_ms = (1000.0 * offsets[0]) // self.sample_rate  # frame_id to duration (ms)
+    # right_blank_duration_ms = (1000.0 * (len(audio) - offsets[-1])) // self.sample_rate
+    # TODO: could use trim_silence() here or a better VAD.
+    audio_voice_only = audio[offsets[0] : offsets[-1]]
+    mfcc = mfcc_fbank(audio_voice_only, sample_rate)
+
+    segments = []
     for i in range(k):
-        end_frame = (i + 1) * num_frames
-        if end_frame < mfcc.shape[0]:
-            frames.append(mfcc[i * num_frames : end_frame])
-        else:
+        start_frame = i * num_frames
+        end_frame = start_frame + num_frames
+
+        if end_frame > mfcc.shape[0]:
             padded = pad_mfcc(mfcc[i * num_frames :], num_frames)
-            frames.append(padded)
+            segments.append(padded)
             break
-    return np.array(frames).reshape(-1, num_frames, 64, 1)
+
+        segments.append(mfcc[start_frame:end_frame])
+
+    return np.array(segments).reshape((-1, 160, 64, 1))
