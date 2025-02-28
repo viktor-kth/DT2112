b6be4690b1d60efc8e79505e8c339bf3b1a00db0
Jan Cap <vorel007@gmail.com>
2025-02-28 10:49:34 +0100

    update embedding strategies

--- src/kth_sr/embeddings/strategies.py
+++ src/kth_sr/embeddings/strategies.py
@@ -6,16 +6,38 @@ import numpy as np
 class BaseEmbeddingStrategy(ABC):
     """Base class for embedding strategies.
     Each audio has multiple windows of embeddings. This class defines how to combine them into a single embedding.
+
+    BaseEmbeddingStrategy has one abstract method `_apply()`
+    which is implemented by the concrete strategies.
     """
 
-    @abstractmethod
     def apply(self, embeddings: np.ndarray) -> np.ndarray:
-        """Apply the strategy.
+        """Apply the strategy and normalize the result.
         Args:
             embeddings (np.ndarray): Array of embeddings.
         Returns:
             np.ndarray: Combined embedding.
         """
+        return self.normalize(self._apply(embeddings))
+
+    @abstractmethod
+    def _apply(self, embeddings: np.ndarray) -> np.ndarray:
+        """Abstract method to implement the strategy.
+
+        Args:
+            embeddings (np.ndarray): Array of embeddings.
+        Returns:
+            np.ndarray: Final embedding after applying the strategy.
+        """
+
+    def normalize(self, embedding: np.ndarray) -> np.ndarray:
+        """Normalize the embedding.
+        Args:
+            embedding (np.ndarray): Embedding to normalize.
+        Returns:
+            np.ndarray: Normalized embedding.
+        """
+        return embedding / np.linalg.norm(embedding)
 
 
 class FirstWindowStrategy(BaseEmbeddingStrategy):
@@ -23,7 +45,7 @@ class FirstWindowStrategy(BaseEmbeddingStrategy):
     This strategy selects the first window of embeddings.
     """
 
-    def apply(self, embeddings: np.ndarray) -> np.ndarray:
+    def _apply(self, embeddings: np.ndarray) -> np.ndarray:
         return embeddings[0]
 
 
@@ -32,5 +54,5 @@ class MeanStrategy(BaseEmbeddingStrategy):
     This strategy calculates the mean of the embeddings.
     """
 
-    def apply(self, embeddings: np.ndarray) -> np.ndarray:
+    def _apply(self, embeddings: np.ndarray) -> np.ndarray:
         return np.mean(embeddings, axis=0)
--- tests/embeddings/test_strategies.py
+++ tests/embeddings/test_strategies.py
@@ -6,8 +6,8 @@ import numpy as np
 @pytest.mark.parametrize(
     "embeddings, expected",
     [
-        (np.array([[1, 2], [3, 4], [5, 6]]), np.array([1, 2])),
-        (np.array([[1, 2], [3, 4], [5, 6], [7, 8]]), np.array([1, 2])),
+        (np.array([[1, 0], [3, 4], [5, 6]]), np.array([1, 0])),
+        (np.array([[0, 2], [3, 4], [5, 6], [7, 8]]), np.array([0, 1])),
     ],
 )
 def test_first_window_strategy(embeddings, expected):
@@ -18,9 +18,10 @@ def test_first_window_strategy(embeddings, expected):
 
 @pytest.mark.parametrize(
     "embeddings, expected",
+    # expected values are normalized embeddings after applying the mean strategy
     [
-        (np.array([[1, 0], [0, 1], [0, -1]]), np.array([0, 1])),
-        (np.array([[1, 2], [3, 4], [5, 6]]), np.array([3, 4])),
+        (np.array([[1, 0], [0, 1], [0, -1]]), np.array([1, 0])),
+        (np.array([[1, 2], [3, 4], [5, 6]]), np.array([0.6, 0.8])),
     ],
 )
 def test_mean_strategy(embeddings, expected):
