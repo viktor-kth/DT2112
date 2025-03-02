6db86f1ca4aff6eeaa0f7799733d56d9710f1d9c
Jan Cap <vorel007@gmail.com>
2025-03-02 21:05:07 +0100

    add file_name and sample_number to get_df by folder function

--- src/kth_sr/utils.py
+++ src/kth_sr/utils.py
@@ -12,11 +12,20 @@ def get_df_by_downloaded_folder(folder: str) -> pd.DataFrame:
     data = []
 
     for file in Path(folder).rglob("*.mp3"):
+        file_name = file.name
         user_id = file.stem.split("_")[0]  # user_id in voxceleb2
+        sample_number = int(file.stem.split("_")[1])
         duration_ms = int(file.stem.split("_")[2])  # duration in ms
 
         # Convert to seconds
         duration_s = duration_ms / 1000.0
-        data.append({"speaker": user_id, "duration_s": duration_s})
+        data.append(
+            {
+                "speaker": user_id,
+                "sample_number": sample_number,
+                "duration_s": duration_s,
+                "file_name": file_name,
+            }
+        )
 
     return pd.DataFrame(data)
